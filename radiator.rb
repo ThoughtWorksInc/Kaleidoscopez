3aa823b5ab6924c5e0e1b975b1e762778c2e5a20
Akshay Mankar <akshaym@thoughtworks.com>
2012-09-20 18:44:16 +0530

    (Akshay/Ashwin) All News Gives out 6 dummy news

--- radiator.rb
+++ radiator.rb
@@ -6,6 +6,11 @@ get '/' do
 end
 
 get '/all_news' do
-  a = [{"title" => "News 1", "Description" => "Desc of N1"},{"title" => "News 2", "Description" => "Desc of N2"}]
+  a = [{"title" => "News 1", "Description" => "Desc of N1"},
+       {"title" => "News 2", "Description" => "Desc of N2"},
+       {"title" => "News 3", "Description" => "Desc of N3"},
+       {"title" => "News 4", "Description" => "Desc of N4"},
+       {"title" => "News 5", "Description" => "Desc of N5"},
+       {"title" => "News 6", "Description" => "Desc of N6"}]
   a.to_json
 end
