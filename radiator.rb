21d1b78ec8596f40383845c2712edc378291f4d7
Akshay Mankar <akshaym@thoughtworks.com>
2012-09-20 11:45:31 +0530

    (Akshay/Ashwin) - Sinatra throws json and index.html catches and displays it using impress.js

--- radiator.rb
+++ radiator.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require "json"
 
 get '/' do
   "Hello World !"
+end
+
+get '/all_news' do
+  a = [{"title" => "News 1", "Description" => "Desc of N1"},{"title" => "News 2", "Description" => "Desc of N2"}]
+  a.to_json
 end
