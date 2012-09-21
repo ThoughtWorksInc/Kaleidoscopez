574dc0f5c76da81119308613285a7e375e3b9bd5
Akshay Mankar <akshaym@thoughtworks.com>
2012-09-21 13:18:25 +0530

    (Ashwin/Akshay) Removed hardcoded news feeds and fetching feeds from db

--- radiator.rb
+++ radiator.rb
@@ -1,18 +1,12 @@
 require 'sinatra/base'
-require "json"
+require_relative 'lib/feed'
+
 class Radiator < Sinatra::Base
   get '/' do
     "Hello World !"
   end
 
   get '/all_news' do
-    a = [{"title" => "News 1", "Description" => "Desc of N1"},
-         {"title" => "News 2", "Description" => "Desc of N2"},
-         {"title" => "News 3", "Description" => "Desc of N3"},
-         {"title" => "News 4", "Description" => "Desc of N4"},
-         {"title" => "News 5", "Description" => "Desc of N5"},
-         {"title" => "News 6", "Description" => "Desc of N6"}]
-    a.to_json
+    Feed.all.to_a.to_json
   end
 end
-Radiator.run!
