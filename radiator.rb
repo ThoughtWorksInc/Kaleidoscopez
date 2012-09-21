a392ecaf221c21c80ae585dbdb378359162b920a
Akshay Mankar <akshaym@thoughtworks.com>
2012-09-21 11:06:56 +0530

    (Ashwin/Akshay) radiator.rb has class Radiator < Sinatra::Base

--- radiator.rb
+++ radiator.rb
@@ -1,16 +1,18 @@
-require 'sinatra'
+require 'sinatra/base'
 require "json"
+class Radiator < Sinatra::Base
+  get '/' do
+    "Hello World !"
+  end
 
-get '/' do
-  "Hello World !"
+  get '/all_news' do
+    a = [{"title" => "News 1", "Description" => "Desc of N1"},
+         {"title" => "News 2", "Description" => "Desc of N2"},
+         {"title" => "News 3", "Description" => "Desc of N3"},
+         {"title" => "News 4", "Description" => "Desc of N4"},
+         {"title" => "News 5", "Description" => "Desc of N5"},
+         {"title" => "News 6", "Description" => "Desc of N6"}]
+    a.to_json
+  end
 end
-
-get '/all_news' do
-  a = [{"title" => "News 1", "Description" => "Desc of N1"},
-       {"title" => "News 2", "Description" => "Desc of N2"},
-       {"title" => "News 3", "Description" => "Desc of N3"},
-       {"title" => "News 4", "Description" => "Desc of N4"},
-       {"title" => "News 5", "Description" => "Desc of N5"},
-       {"title" => "News 6", "Description" => "Desc of N6"}]
-  a.to_json
-end
+Radiator.run!
