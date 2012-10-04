6f33b1c973798f6291ddaa9d38800a5678b59b36
Sonia Ghanekar <soniaghanekar@gmail.com>
2012-10-04 18:28:48 +0530

    [Akshay/Sonia]: Added XKCD in seeds, removed hacker news

--- db/seed.rb
+++ db/seed.rb
@@ -2,7 +2,7 @@ require './config/boot'
 
 Feed.delete_all
 Item.delete_all
-Rss.create({:name => "Hacker News", :url => "http://news.ycombinator.com/rss"})
+Rss.create({:name => "XKCD", :url => "http://xkcd.com/rss.xml"})
 Atom.create({:name => "Linus Torvald's Blog", :url => "http://torvalds-family.blogspot.com/feeds/posts/default"})
 RssFeedBurner.create({:name => "Tech Crunch", :url => "http://feeds.feedburner.com/TechCrunch/"})
 RssFeedBurner.create({:name => "ThoughtWorks Events", :url => "http://feeds.feedburner.com/thoughtworks-upcoming-events"})
