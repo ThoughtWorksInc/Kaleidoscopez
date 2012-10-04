3fa7fbeb2c7eb86c31ae99d5bf80f2409cb568cb
Sonia Ghanekar <soniaghanekar@gmail.com>
2012-10-04 17:32:45 +0530

    [Akshay/Sonia] : Added date to rss (changed spec)

--- spec/app/models/rss_spec.rb
+++ spec/app/models/rss_spec.rb
@@ -16,11 +16,13 @@ describe Rss do
     @feedzirra_rss.entries[0].title = "First Post"
     @feedzirra_rss.entries[0].url = "test.url"
     @feedzirra_rss.entries[0].author = "Dave Thomas"
+    @feedzirra_rss.entries[0].published = "2012-09-28 06:03:48 UTC"
     @feedzirra_rss.entries[0].summary = "<div><br /></div><div>(Side note: test)<br /><div><br /></div><div>test1</div><div><br /></div><div>Because we <i>care just enough</i> to feed our kids!</div><div><br /></div><div>test2</div></div><div class=\"blogger-post-footer\"><img width='1' height='1' src='http://test.com' alt='' /></div>"
     @feedzirra_rss.entries[1].summary = "<div><br /></div><div>(Side note: test)<br /><div><br /></div><div>test1</div><div><br /></div><div>Because we <i>care just enough</i> to feed our kids!</div><div><br /></div><div>test2</div></div><div class=\"blogger-post-footer\"><img width='1' height='1' src='http://test1.com' alt='' /></div>"
     @feedzirra_rss.entries[1].title = "Second Post"
     @feedzirra_rss.entries[1].url = "test1.url"
     @feedzirra_rss.entries[1].author = "Daven Thomas"
+    @feedzirra_rss.entries[1].published = "2010-09-28 06:03:48 UTC"
   end
   
   it "should fetch feeds as items" do
@@ -30,13 +32,13 @@ describe Rss do
     items[0].title.should == "First Post"
     items[0].url.should == "test.url"
     items[0].author.should == "Dave Thomas"
-    items[0].date.should == nil
+    items[0].date.should ==  "2012-09-28"
     items[0].image.should == "http://test.com"
 
     items[1].title.should == "Second Post"
     items[1].url.should == "test1.url"
     items[1].author.should == "Daven Thomas"
-    items[1].date.should == nil
+    items[1].date.should == "2010-09-28"
     items[1].image.should == "http://test1.com"
 
   end
