981f8d1f710be8dc1932dd93fd5ba1f81ea24a2f
Sonia Ghanekar <soniaghanekar@gmail.com>
2012-10-05 12:30:43 +0530

    [Yahya/Sonia]: Changed feeds in seed.db

--- db/seed.rb
+++ db/seed.rb
@@ -3,6 +3,6 @@ require './app/boot'
 Feed.delete_all
 Item.delete_all
 Feed.create({:name => "XKCD", :url => "http://xkcd.com/rss.xml"})
-Feed.create({:name => "Linus Torvald's Blog", :url => "http://torvalds-family.blogspot.com/feeds/posts/default"})
 Feed.create({:name => "Tech Crunch", :url => "http://feeds.feedburner.com/TechCrunch/"})
 Feed.create({:name => "ThoughtWorks Events", :url => "http://feeds.feedburner.com/thoughtworks-upcoming-events"})
+Feed.create({:name => "ThoughtWorks Blogs", :url => "http://www.thoughtworks.com/blogs/rss/current"})
