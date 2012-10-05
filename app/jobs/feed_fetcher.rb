ef2f8bbee83d1378b9b9fb4e93ebb063285f4de0
Yahya <yahyapo@thoughtworks.com>
2012-10-05 17:13:36 +0530

    [Ashwin] Reverting back to using slice for getting top 10 items from every source

--- app/jobs/feed_fetcher.rb
+++ app/jobs/feed_fetcher.rb
@@ -1,11 +1,19 @@
 class FeedFetcher
 
-  def self.get_all_feeds
-    items = Feed.all.collect { |feed| feed.fetch_feed_entries }.flatten.compact
-    return if items.length < 10
+  NUMBER_OF_ITEMS = 15
+
+  def self.get_all_feeds()
+    items = Feed.all.collect { |feed| feed.fetch_feed_entries.slice(0,NUMBER_OF_ITEMS) }.flatten.compact
+
+    return if (items_are_too_few?(items))
 
     Item.delete_all
     items.each { |item| item.save }
+  end
+
+  private
+  def self.items_are_too_few?(items)
+    items.length < 10
   end
 
 end
