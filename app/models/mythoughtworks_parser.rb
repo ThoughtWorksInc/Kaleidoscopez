4780044c884a2ed343b729bb083dc5f9c999cf4f
Sonia Ghanekar <soniaghanekar@gmail.com>
2012-10-18 14:44:11 +0530

    [Sonia]: Added images for MyThoughtworks blog posts

--- app/models/mythoughtworks_parser.rb
+++ app/models/mythoughtworks_parser.rb
@@ -1,15 +1,47 @@
+
 class MythoughtworksParser
 
   def create_item(post, source)
+    image = get_image(post)
     Item.new({
       :title => post["subject"],
       :url => post["resources"]["self"]["ref"],
       :author => post["author"]["name"],
       :author_image => post["author"]["resources"]["avatar"]["ref"],
       :date => post["modificationDate"],
-      :image => nil,
+      :image => image,
       :source => source
     })
+  end
+
+  private
+
+  def get_image(post)
+    options = {
+        :basic_auth => {
+            :username => ENV['TW_USERNAME'],
+            :password => ENV['TW_PASSWORD']
+        }
+    }
+    get_image_from_content_url(options, post["resources"]["self"]["ref"])
+  end
+
+  def get_image_from_content_url(options, content_url)
+    content = JSON::parse((MyThoughtworks.get(content_url, options)).parsed_response)
+    images = JSON::parse((MyThoughtworks.get(content["resources"]["images"]["ref"], options)).parsed_response)
+    get_biggest_image(images)
+  end
+
+  def get_biggest_image(images)
+    biggest_image_size = 0
+    biggest_image_url = nil
+    images.each do |image|
+       if((image["size"]).to_i > biggest_image_size)
+         biggest_image_size = image["size"].to_i
+         biggest_image_url = image["ref"]
+       end
+    end
+    biggest_image_url
   end
 
 end
--- spec/models/mythoughtworks_parser_spec.rb
+++ spec/models/mythoughtworks_parser_spec.rb
@@ -2,22 +2,17 @@ require "spec_helper"
 
 describe MythoughtworksParser do
 
-  it "should authenticate user and fetch items" do
-    source = Source.new
+  before do
+    @source = Source.new
 
-    item = MythoughtworksParser.new.create_item(mythoughtworks_post, source)
+    @options = {
+        :basic_auth => {
+            :username => ENV['TW_USERNAME'],
+            :password => ENV['TW_PASSWORD']
+        }
+    }
 
-    item.title.should == "My First Post"
-    item.url.should == "http://blogpost.api.url"
-    item.date.should == "2012-03-02T05:15:32.743+0000"
-    item.author.should == "Yahya Poonawala"
-    item.image.should == nil
-    item.source.should == source
-    item.author_image.should == "http://yahya.avatar.me"
-  end
-
-  def mythoughtworks_post
-    {
+    @mythoughtworks_post = {
         "subject" => "My First Post",
         "author" => {
             "name" => "Yahya Poonawala",
@@ -34,6 +29,65 @@ describe MythoughtworksParser do
         },
         "modificationDate" => "2012-03-02T05:15:32.743+0000"
     }
+
+
+    @post_content = {
+            "resources" => {
+                "images" => {
+                    "ref" => "content_images_url"
+                }
+            }
+    }
+
+    @images =[{
+          "size" => "100",
+          "ref" => "image1_url"
+        },
+        {
+          "size" => "200",
+          "ref" => "image2_url"
+        }
+    ]
+
+    @httparty_content = mock(HTTParty)
+    @httparty_images = mock(HTTParty)
   end
+
+  it "should authenticate user and return item with nil image when no image exists" do
+    MyThoughtworks.should_receive(:get).with("http://blogpost.api.url",@options).and_return(@httparty_content)
+    @httparty_content.should_receive(:parsed_response).and_return(@post_content.to_json)
+    images = []
+    MyThoughtworks.should_receive(:get).with("content_images_url", @options).and_return(@httparty_images)
+    @httparty_images.should_receive(:parsed_response).and_return(images.to_json)
+
+    item = MythoughtworksParser.new.create_item(@mythoughtworks_post, @source)
+
+    item.title.should == "My First Post"
+    item.url.should == "http://blogpost.api.url"
+    item.date.should == "2012-03-02T05:15:32.743+0000"
+    item.author.should == "Yahya Poonawala"
+    item.image.should == nil
+    item.source.should == @source
+    item.author_image.should == "http://yahya.avatar.me"
+  end
+
+  it "should authenticate user and return item with biggest image" do
+    MyThoughtworks.should_receive(:get).with("http://blogpost.api.url",@options).and_return(@httparty_content)
+    @httparty_content.should_receive(:parsed_response).and_return(@post_content.to_json)
+    MyThoughtworks.should_receive(:get).with("content_images_url", @options).and_return(@httparty_images)
+    @httparty_images.should_receive(:parsed_response).and_return(@images.to_json)
+
+    item = MythoughtworksParser.new.create_item(@mythoughtworks_post, @source)
+
+    item.title.should == "My First Post"
+    item.url.should == "http://blogpost.api.url"
+    item.date.should == "2012-03-02T05:15:32.743+0000"
+    item.author.should == "Yahya Poonawala"
+    item.image.should == "image2_url"
+    item.source.should == @source
+    item.author_image.should == "http://yahya.avatar.me"
+  end
+
+
 
 end
