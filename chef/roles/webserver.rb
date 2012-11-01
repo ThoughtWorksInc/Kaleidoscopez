926bfbf0ba10f724efdc919a98fc381510a77e40
Akshay Mankar <akshaym@thoughtworks.com>
2012-11-01 17:10:28 +0530

    (Akshay/Ashwin) Latest version of rvm(1.16.17) failing with the cookbook, so installing 1.16.16

--- chef/roles/webserver.rb
+++ chef/roles/webserver.rb
@@ -2,6 +2,8 @@ name "webserver"
 description "Role to setup and install web server environment"
 run_list "recipe[apt]", "recipe[git]", "recipe[rvm::system]", "recipe[mongodb::10gen_repo]", "recipe[mongodb::default]", "recipe[libcurl]", "recipe[wkhtmltoimage]", "recipe[cron]"
 override_attributes "rvm" => {
+    "version" => "1.16.16",
+    "branch" => "none",
     "rubies" => ['1.9.3-p194'],
     "default_ruby" => "1.9.3-p194",
     "global_gems" => [
