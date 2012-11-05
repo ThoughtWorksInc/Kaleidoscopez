500bce03bfbf4f148b9b112c6d3ef4a62d138c69
Akshay Mankar <akshaym@thoughtworks.com>
2012-11-05 19:25:35 +0530

    (Akshay) Stop tar from printing all files while bootstraping

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ scp /tmp/chef.tar.gz $USERNAME@$IP_ADDR:/tmp
 ssh $USERNAME@$IP_ADDR '
 cd /tmp
 rm -rf chef kaleidoscopez
-tar -xvf chef.tar.gz
+tar -xf chef.tar.gz
 mv chef kaleidoscopez
 '
 knife bootstrap $IP_ADDR -r 'role[webserver]' -x $USERNAME -P $PASSWORD --template-file bootstrap/chef-full.erb --sudo
