e91d4a085428d8fa5110dfe3ef7d621c554adcf6
Akshay Mankar <akshaym@thoughtworks.com>
2012-11-05 17:46:18 +0530

    (Akshay)Setting cronjob to run once per hour

--- chef/site-cookbooks/cron/recipes/default.rb
+++ chef/site-cookbooks/cron/recipes/default.rb
@@ -1,4 +1,4 @@
 cron "fetch new items" do
-    minute "*/10"
+    minute "0"
     command "bash --login -c 'cd /opt/Kaleidoscopez/current/; export TWITTER_CUSTOMER_KEY=#{ENV['TWITTER_CUSTOMER_KEY']}; export TWITTER_CUSTOMER_SECRET=#{ENV['TWITTER_CUSTOMER_SECRET']}; export TWITTER_OAUTH_TOKEN=#{ENV['TWITTER_OAUTH_TOKEN']}; export TWITTER_OAUTH_TOKEN_SECRET=#{ENV['TWITTER_OAUTH_TOKEN_SECRET']}; export TW_USERNAME=#{ENV['TW_USERNAME']}; export TW_PASSWORD=#{ENV['TW_PASSWORD']};ruby app/jobs/clock.rb'"
 end
